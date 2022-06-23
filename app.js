a6e07ab9fb19c844ba401d4928b8a299c88256f0
jad amine <jade1992amine@gmail.com>
2022-06-23 12:54:38 +0300

    Fetch all data from collection as cursor

--- app.js
+++ app.js
@@ -11,10 +11,21 @@ connectToDb((err) => {
     app.listen(3000, () => {
       console.log("app listening to port 3000");
     });
-    db = getDb();     // object to interact with db
+    db = getDb(); // object to interact with db
   }
 });
 
-app.get("/blogs", (req, res) => {
-  res.json({ message: "welcome" });
+app.get("/books", (req, res) => {
+  // to interact with a collection
+  let books = [];
+  db.collection("books")
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))   // cursor methods (forEach or Array);
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "My Server error" });
+    });
 });
